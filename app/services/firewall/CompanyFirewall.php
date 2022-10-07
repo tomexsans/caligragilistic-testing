909664321a8b5fa60dfd2c68c6617a983ddb8c57
Tomexsans Evaristo <t2t.exe@gmail.com>
2022-10-07 20:12:48 +0800

    Added Firewall

---
+++ app/services/firewall/CompanyFirewall.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+PHP Web Application Firewall (WAF)
+
+    Robust community-powered security engine
+    Real-time SQLi, XSS, LFI & 100+ threats protection
+    Malware scanning & removal
+    Facility of blocking Bad bots, Country, IP range, automated vulnerability scanners, Fake search engine bots
+    Malicious file upload prevention
+    Admin login activity logging
+    Admin brute force protection
+    File Injection/Webshell protection
+    Code Injection protection
+    Directory traversal protection
+    Automatic blocking of known hackers
+    Layer 7 DDoS protection
+    Smart honeypot system to trap hackers
+    Rate limit web requests
+    Automatic spam blocking
+    Content stealing & scraping prevention
+    Htaccess security
+    No latency (Our security engine runs on your server & only takes 0.002s to detect threats)
+*/
